ede31bf0c296615735cd0cac4d3a9145e41fb998
TaghreedSweideh <taghreedswidah9@gmail.com>
2024-11-11 06:14:49 +0300

    first commit

---
+++ src/main/java/com/mycompany/main/GamePlaying/Player.java
@@ -0,0 +1,83 @@
+package com.mycompany.main.GamePlaying;
+
+import com.mycompany.main.GameStructure.Action;
+import com.mycompany.main.GameStructure.GameState;
+import java.util.ArrayList;
+import javax.swing.text.Position;
+
+/**
+ *
+ * @author taghr
+ */
+import java.util.Scanner;
+
+public class Player {
+
+    private GameState gameState;
+    private final Action action;
+
+    public Player(GameState gameState) {
+        this.gameState = gameState;
+        this.action = new Action();
+    }
+
+    public void startGame() {
+        Scanner scanner = new Scanner(System.in);
+        boolean gameWon = false;
+
+        while (!gameWon) {
+            gameState.printGame(gameState.getBoard());
+            System.out.println("To move use: \nW (up) \nA (left) \nS (down) \nD (right)");
+
+            char input = scanner.next().toUpperCase().charAt(0);
+            gameWon = keyAction(input);
+        }
+        gameState.printGame(gameState.getBoard());
+        System.out.println("************ CONGATULATIONS ************");
+        System.out.println("************     YOU WON     ************");
+        scanner.close();
+    }
+
+    private boolean keyAction(char input) {
+        switch (input) {
+            case 'W':
+                if (action.canMoveUp(gameState)) {
+                    gameState = action.moveUp(gameState);
+
+                } else {
+                    System.out.println("Cannot move up.");
+                }
+                break;
+            case 'A':
+                if (action.canMoveLeft(gameState)) {
+                    gameState = action.moveLeft(gameState);
+
+                } else {
+                    System.out.println("Cannot move left.");
+                }
+                break;
+            case 'S':
+                if (action.canMoveDown(gameState)) {
+                    gameState = action.moveDown(gameState);
+
+                } else {
+                    System.out.println("Cannot move down.");
+                }
+                break;
+            case 'D':
+                if (action.canMoveRight(gameState)) {
+                    gameState = action.moveRight(gameState);
+
+                } else {
+                    System.out.println("Cannot move right.");
+                }
+                break;
+            default:
+                System.out.println("Invalid input. Use W, A, S, or D.");
+                break;
+        }
+
+        // Check if the player has won after the move
+        return gameState.checkWin(gameState.getColoredSquares());
+    }
+}
---
+++ src/main/java/com/mycompany/main/GameStructure/GameState.java
@@ -0,0 +1,148 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.main.GameStructure;
+
+import com.mycompany.main.GameStructure.Position;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ *
+ * @author taghr
+ */
+public class GameState {
+
+    char[][] board;
+    List<Position> coloredSquares;
+    List<Position> goalSquares;
+
+    public GameState(char[][] board, List<Position> coloredSquares, List<Position> goalSquared) {
+        this.board = board;
+        this.coloredSquares = coloredSquares;
+        this.goalSquares = goalSquared;
+    }
+
+    public GameState() {
+    }
+
+    public char[][] getBoard() {
+        return board;
+    }
+
+    public void setBoard(char[][] board) {
+        this.board = board;
+    }
+
+    public List<Position> getColoredSquares() {
+        return coloredSquares;
+    }
+
+    public void setColoredSquares(List<Position> coloredSquares) {
+        this.coloredSquares = coloredSquares;
+    }
+
+    public List<Position> getGoalSquared() {
+        return goalSquares;
+    }
+
+    public void setGoalSquared(List<Position> goalSquared) {
+        this.goalSquares = goalSquared;
+    }
+
+    public void printGame(char[][] board) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                System.out.print(" " + board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public boolean checkWin(List<Position> coloredSquares) {
+        return coloredSquares.isEmpty();
+    }
+
+    public List<GameState> states(GameState currentGame) {
+        List<GameState> possibleStates = new ArrayList<>();
+        Action action = new Action();
+        if (action.canMoveLeft(currentGame)) {
+            possibleStates.add(action.moveLeft(currentGame));
+        }
+        if (action.canMoveRight(currentGame)) {
+            possibleStates.add(action.moveRight(
+                    currentGame));
+        }
+        if (action.canMoveUp(currentGame)) {
+            possibleStates.add(action.moveUp(currentGame));
+        }
+        if (action.canMoveDown(currentGame)) {
+            possibleStates.add(action.moveDown(currentGame));
+        }
+        return possibleStates;
+
+    }
+
+    public GameState deepCopy() {
+        GameState newGameState = new GameState();
+       
+        newGameState.board = new char[this.board.length][this.board[0].length];
+        for (int i = 0; i < this.board.length; i++) {
+            newGameState.board[i] = this.board[i].clone();
+        }
+        newGameState.coloredSquares = new ArrayList<>();
+        for (Position pos : this.coloredSquares) {
+            newGameState.coloredSquares.add(new Position(pos.getRow(), pos.getCol(), pos.getColor()));
+        }
+
+        newGameState.goalSquares = new ArrayList<>();
+        for (Position pos : this.goalSquares) {
+            newGameState.goalSquares.add(new Position(pos.getRow(), pos.getCol(), pos.getColor()));
+        }
+
+        return newGameState;
+    }
+    
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        GameState gameState = (GameState) o;
+        return Arrays.deepEquals(board, gameState.board);  // Compare the board states
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.deepHashCode(board);  // Use the board state for hashing
+    }
+
+//    @Override
+//    public boolean equals(Object o) {
+//        if (this == o) return true;
+//        if (o == null || getClass() != o.getClass()) return false;
+//        GameState gameState = (GameState) o;
+//
+//        // Check if the boards are identical
+//        if (!Arrays.deepEquals(board, gameState.board)) {
+//            return false;
+//        }
+//
+//        // Check if the colored squares are identical
+//        return Objects.equals(coloredSquares, gameState.coloredSquares);
+//    }
+//
+//    @Override
+//    public int hashCode() {
+//        // Combine the hash codes of the board and colored squares
+//        int result = Arrays.deepHashCode(board);
+//        result = 31 * result + Objects.hashCode(coloredSquares);
+//        return result;
+//    }
+}
